f5fb0257857e319e0c3c101ca20cba67cf2ff9a9
Sophia Ko <s88431ss@yahoo.com.tw>
2024-10-30 10:42:39 +0800

    feat: 要開始找新工作了，微調內容及樣式

--- js/main.js
+++ js/main.js
@@ -1,66 +1,71 @@
-
 $(function () {
- 
-    // 緩慢回到頁面最上方的按鈕
-    $('#gotop').click(function () {
-   
-     var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');
-     $body.animate({
-      scrollTop: 0
-     }, 600);
-   
-     return false;
-   
-    });
-   
-    // 點擊超連結，緩慢滾動到錨點
-    $("a.anchor").click(function () {
-     console.log($(this).attr("href"))
-   
-     var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');
-   
-     $body.animate({
-      scrollTop: $($(this).attr("href")).offset().top + "px"
-     }, {
-      duration: 800,
-      easing: "swing"
-     });
-   
-     return false;
-   
-    });
-   
-});
+  // 緩慢回到頁面最上方的按鈕
+  $("#gotop").click(function () {
+    var $body = window.opera
+      ? document.compatMode == "CSS1Compat"
+        ? $("html")
+        : $("body")
+      : $("html,body");
+    $body.animate(
+      {
+        scrollTop: 0,
+      },
+      600
+    );
 
+    return false;
+  });
+
+  // 點擊超連結，緩慢滾動到錨點
+  $("a.anchor").click(function () {
+    console.log($(this).attr("href"));
+
+    var $body = window.opera
+      ? document.compatMode == "CSS1Compat"
+        ? $("html")
+        : $("body")
+      : $("html,body");
+
+    $body.animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top + "px",
+      },
+      {
+        duration: 800,
+        easing: "swing",
+      }
+    );
+
+    return false;
+  });
+});
 
 // ------- wow.js + animate.css ----------
 
-
 new WOW({
-    boxClass: 'wow',
-    animateClass: 'animate__animated',
+  boxClass: "wow",
+  animateClass: "animate__animated",
 }).init();
 
-
 var menuVue = new Vue({
-    el: '#menu',
-    data(){
-        return {
-            burgerActive: false,
-            list: [
-                {
-                    url: 'index.html', title: '個人介紹',
-                    icon: 'icon-home', en: 'HOME',
-                },
-                {
-                    url: 'portfolio.html', title: '作品一覽',
-                    icon: 'icon-portfolio', en: 'PROTFOLIO',
-                },
-                // {
-                //     url: 'note.html', title: '學習筆記',
-                //     icon: 'icon-note', en: 'NOTE',
-                // },
-            ],
-        }
-    }
+  el: "#menu",
+  data() {
+    return {
+      burgerActive: false,
+      list: [
+        {
+          url: "index.html",
+          title: "個人介紹",
+          icon: "icon-home",
+          en: "HOME",
+        },
+        {
+          url: "portfolio.html",
+          title: "作品一覽",
+          icon: "icon-portfolio",
+          en: "PROTFOLIO",
+        },
+      ],
+    };
+  },
 });
